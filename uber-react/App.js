876f2743fe5de85524ca4289520942ce906ec900
DevAyeong <devayeong@gmail.com>
2022-06-26 23:09:04 +0900

    NavOptions 퍼블리싱

--- uber-react/App.js
+++ uber-react/App.js
@@ -2,13 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { store } from './store';
 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- uber-react/screens/HomeScreen.js
+++ uber-react/screens/HomeScreen.js
@@ -1,21 +1,24 @@
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View>
+      <View style={tw`p-5`}>
           <Image
           style={{
-              width: 120, 
-              height: 120, 
+              width: 100, 
+              height: 100, 
               resizeMode: 'contain',
           }}
             source={{
-                uri: "https://brandlogos.net/wp-content/uploads/2021/12/uber-brandlogo.net_.png",
+                uri: "https://links.papareact.com/gzs",
             }}
           />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   )
