5972d074dfcf3e5ba453aabf582b5efc48457df7
DevAyeong <devayeong@gmail.com>
2022-06-27 15:07:45 +0900

    구글 API 설정, MapScreen 퍼블리싱

--- uber-react/slices/navSlice.js
+++ uber-react/slices/navSlice.js
@@ -1,32 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
     origin: null,
     destination: null,
-    travelTimeInformation: null
+    travelTimeInformation: null,
 }
 
-export const navSlice = createSlice({
-    name: 'nav',
-    initialState,
-    reducer: {
-        setOrigin: (state, action) => {
-            state.origin = action.payload;
-        },
-        setDestination: (state, action) => {
-            state.destination = action.payload;
-        },
-        setTravelTimeInformation: (state, action) => {
-            state.travelTimeInformation = action.payload;
-        },
+const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+        state.origin = action.payload
     },
-});
+    setDestination: (state, action) => {
+        state.destination = action.payload
+    },
+    setTravelTimeInformation: (state, action) => {
+        state.travelTimeInformation = action.payload
+    },
+  }
+})
 
-export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
+export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions
 
-// Selectors
-export const selectOrigin = (state) => state.nav.origin;
-export const selectDestination = (state) => state.nav.destination;
-export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+export const selectOrigin = state => state.nav.origin
+export const selectDestination = state => state.nav.destination
+export const selectTravelTimeInformation = state => state.nav.travelTimeInformation
 
-export default navSlice.reducer;
+export default navSlice.reducer
