2c441997690372d58f501284f6c3c9730fe5ad02
DevAyeong <devayeong@gmail.com>
2022-06-27 20:46:41 +0900

    TravelTime Price 계산 기능 구현

--- uber-react/components/RideOptionsCard.js
+++ uber-react/components/RideOptionsCard.js
@@ -1,89 +1,112 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import tw from 'tailwind-react-native-classnames'
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
-import { Icon, Image } from 'react-native-elements'
-import { useNavigation } from '@react-navigation/native'
+import React, { useState } from 'react';
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectTravelTimeInformation } from '../slices/navSlice';
 
 const data = [
-    {
-        id: "Uber-X-123",
-        title: "UberX",
-        multiplier: 1,
-        image:
-            "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberX.png",
-    },
-    {
-        id: "Uber-XL-456",
-        title: "UberXL",
-        multiplier: 1.2,
-        image:
-            "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberXL.png",
-    },
-    {
-        id: "Uber-LUX-789",
-        title: "UberLUX",
-        multiplier: 1.75,
-        image:
-            "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/Lux.png",
-    },
+  {
+    id: 'Uber-X-123',
+    title: 'Uber X',
+    multiplier: 1,
+    image: 'https://links.papareact.com/3pn',
+  },
+  {
+    id: 'Uber-XL-456',
+    title: 'Uber XL',
+    multiplier: 1.2,
+    image: 'https://links.papareact.com/5w8',
+  },
+  {
+    id: 'Uber-LUX-789',
+    title: 'Uber LUX',
+    multiplier: 1.75,
+    image: 'https://links.papareact.com/7pf',
+  },
 ];
 
+// If we have SURGE pricing, this goes up
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
-    const navigation = useNavigation();
-    const [selected, setSelected] = useState(null);
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
-    <View style={tw`bg-white flex-1`}>
-        <View>
-            <TouchableOpacity
-            onPress={() => navigation.navigate("NavigateCard")}
-            style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
-            >
-            <Icon name='chevron-left' type='fontawesome' />
-            </TouchableOpacity>
-            <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
-        </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('NavigateCard')}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}>
+          <Icon name='chevron-left' type='fontawesome' />
+        </TouchableOpacity>
+        <Text style={tw`text-center p-5 text-xl`}>
+          Select a Ride - {travelTimeInformation?.distance?.text}
+        </Text>
+      </View>
 
-        <FlatList 
-            data={data}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item: { id, title, multiplier, image}, item }) => (
-                <TouchableOpacity
-                    onPress={() => setSelected(item)} 
-                    style={tw`flex-row justify-between items-center px-10 ${
-                        id === selected?.id && "bg-gray-200"
-                    }`}
-                >
-                    <Image
-                        style={{
-                            width: 100,
-                            height: 100,
-                            resizeMode: "contain",
-                        }}
-                        source={{ uri: image }}
-                    />
-                    <View style={tw`-ml-6`}>
-                        <Text style={tw`text-xl font-semibold`}>{title}</Text>
-                        <Text>Travel Time...</Text>
-                    </View>
-                    <Text style={tw`text-xl`}>$99</Text>
-                </TouchableOpacity>
-            )}
-        />
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+              id === selected?.id && 'bg-gray-200'
+            }`}>
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: 'contain',
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>약 {travelTimeInformation?.duration?.text} 소요</Text>
+            </View>
+            <Text style={tw`text-xl`}>
+              {new Intl.NumberFormat('en-us', {
+                style: 'currency',
+                currency: 'USD',
+              }).format(
+                (travelTimeInformation?.duration.value *
+                  SURGE_CHARGE_RATE *
+                  multiplier) /
+                  100
+              )}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
 
-        <View>
-            <TouchableOpacity 
-                disabled={!selected} 
-                style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
-            >
-                <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-  )
-}
+      <View style={tw`mt-auto border-t border-gray-300`}>
+        <TouchableOpacity
+          disabled={!selected}
+          style={tw`bg-black py-3 m-3 rounded-full ${
+            !selected && 'bg-gray-300'
+          }`}>
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default RideOptionsCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
